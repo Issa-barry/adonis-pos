738e015bfd672df3db6bb76835e7bb36ab388226
Issa-barry <issabarry67@gmail.com>
2025-11-30 14:49:50 +0100

    maj

--- inertia/app/app.ts
+++ inertia/app/app.ts
@@ -1,31 +1,42 @@
 /// <reference path="../../adonisrc.ts" />
 /// <reference path="../../config/inertia.ts" />
 
-import '../css/app.css';
-import { createSSRApp, h } from 'vue'
-import type { DefineComponent } from 'vue'
+import '../css/app.css'
+import { createSSRApp, h, type DefineComponent } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { resolvePageComponent } from '@adonisjs/inertia/helpers'
 
+// 🔹 PrimeVue
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+
+
 const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS'
 
-createInertiaApp({
+createInertiaApp<DefineComponent>({
   progress: { color: '#5468FF' },
-
   title: (title) => `${title} - ${appName}`,
 
   resolve: (name) => {
-    return resolvePageComponent(
-      `../pages/${name}.vue`,
-      import.meta.glob<DefineComponent>('../pages/**/*.vue'),
-    )
+    const pages = import.meta.glob<DefineComponent>('../pages/**/*.vue')
+
+    const page: any = resolvePageComponent(`../pages/${name}.vue`, pages)
+
+    return page
   },
 
   setup({ el, App, props, plugin }) {
-    
-    createSSRApp({ render: () => h(App, props) })
-    
+    const vueApp = createSSRApp({ render: () => h(App, props) })
+
+    vueApp
       .use(plugin)
+      .use(PrimeVue, {
+        theme: {
+          preset: Aura,
+        },
+      })
       .mount(el)
   },
 })
